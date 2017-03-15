881ffc389a7fed5b483528f31eb62b2dc2284857
Devashen Govender <govenderdevashen9@gmail.com>
2017-03-15 10:16:16 +0200

    Add project files.

---
+++ TSAR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TSAR.Startup))]
+namespace TSAR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
