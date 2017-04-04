ddd70afcc6573dcea12691d83200acbec3871750
Devashen Govender <govenderdevashen9@gmail.com>
2017-04-04 13:06:30 +0200

    Updated Layout for Contact us

--- TSAR/Controllers/HomeController.cs
+++ TSAR/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TSAR.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace TSAR.Controllers
 {
@@ -20,10 +24,46 @@ namespace TSAR.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("dotcomdevelopers19@gmail.com"));  // replace with valid value 
+                message.From = new MailAddress(model.FromEmail);  // replace with valid value
+                message.Subject = "Message from TSAR website";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "dotcomdevelopers19@gmail.com",  // replace with valid value
+                        Password = "P@ssword0123"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        public ActionResult Sent()
+        {
             return View();
         }
     }
---
+++ TSAR/Models/ContactEmailFormModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TSAR.Models
+{
+    public class EmailFormModel
+    {
+        [Required, Display(Name = "Your Name")]
+        public string FromName { get; set; }
+
+        [Required, Display(Name = "Your Email"), EmailAddress]
+        public string FromEmail { get; set; }
+
+        [Required, Display(Name = "Your Phone "), Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
