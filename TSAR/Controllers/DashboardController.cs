774c12877e4be17c67d1975728ae28f1fd5ffdc6
ShriyaBramdeo <shriyabramdeo@gmail.com>
2017-05-09 19:02:43 +0200

    IM Chat
    Created Controller(Dashboard)
    Added snippet of code to every html page.
    Added view page(Dashboard) with iframe

---
+++ TSAR/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TSAR.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        // GET: Dashboard
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
