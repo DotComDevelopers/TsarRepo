141b7610766dfc20f38bf5c4d6c47aa6331caf26
Jonathan <jonathan.jagarath05@gmail.com>
2017-05-14 00:26:22 +0200

    Create Ticket

---
+++ TSAR/Models/Ticket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TSAR.Models
+{
+    public class Ticket
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "Client Name")]
+        public string ClientName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Fault Description")]
+        public string FaultDescription { get; set; }
+
+        [Display(Name = "Priority")]
+        public string Priority { get; set; }
+
+
+        [Required]
+        [Display(Name = "Date & Time")]
+        public DateTime Date { get; set; }
+    }
+}
