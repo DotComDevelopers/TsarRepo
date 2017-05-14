325ebad335452a51f776336d44dcdb309a326d7d
Aleysha2203 <aleysha2203@gmail.com>
2017-05-14 03:25:14 +0100

    Added Sms functionality, Added Twilio nugget package reference

---
+++ TSAR/Controllers/SmsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+using TSAR.SmsHelper;
+
+namespace TSAR.Controllers
+{
+    public class SmsController : Controller
+    {
+        // GET: Sms
+        //public  ActionResult SmsView()
+        //{
+        //    return View();
+        //}
+
+        public void SmsView()
+        {
+            
+            var restCliennt = new TwilioSmsRestClient();
+          var result =   restCliennt.SendMessage("","");
+        }
+
+    }
+}
---
+++ TSAR/SmsHelper/TwilioSmsRestClient.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Twilio.Clients;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace TSAR.SmsHelper
+{
+    public class TwilioSmsRestClient : ITwilioSmsRestClient
+    {
+        private readonly ITwilioRestClient _client;
+
+        public TwilioSmsRestClient()
+        {
+            _client = new TwilioRestClient("AC92899c64b86aefec4836139d443487ad", "c32f724903485277ecc816e298169f27");
+        }
+
+        public async Task<MessageResource> SendMessage(string text, string sendToNumber)
+        {
+            sendToNumber = "+27711919156";
+
+            var toPhoneNumber = new PhoneNumber(sendToNumber);
+            return await MessageResource.CreateAsync(
+                toPhoneNumber,
+                @from: new PhoneNumber("+12522854117"),
+                body: text,
+                mediaUrl: null,
+                client: _client);
+
+
+        }
+
+    }
+
+}
