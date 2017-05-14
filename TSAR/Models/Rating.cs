f2b03d7e316677c5e8188f592d75546cc819f138
Shirdika Beerajh <cbass1130@gmail.com>
2017-05-14 16:01:10 +0200

    Rating -Done by Jonathan Jagarath

---
+++ TSAR/Models/Rating.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+
+namespace TSAR.Models
+{
+    public class Rating
+    {
+        [Key]
+        [Required]
+        [Display(Name = "Rating id")]
+        public int RatingId { get; set; }
+        public virtual Consultant Consultant { get; set; }
+        public int ConsultantNum { get; set; }
+        [Required]
+        [Display(Name = "Rating")]
+        public string Rate { get; set; }
+        [Required]
+        [Display(Name = "Reason")]
+        public string Reason { get; set; }
+        [Required]
+        [Display(Name = "Client Name")]
+        public string ClientName { get; set; }
+    }
+}
