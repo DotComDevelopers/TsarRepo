76cfb0059aaec5877c0c78706446a758075d2312
ShriyaBramdeo <shriyabramdeo@gmail.com>
2017-08-24 21:31:45 +0200

    Added Images, changed background

--- MobileTsar/MobileTsar.Droid/MainActivity.cs
+++ MobileTsar/MobileTsar.Droid/MainActivity.cs
@@ -11,7 +11,7 @@ using Plugin.Fingerprint;
 
 namespace MobileTsar.Droid
 {
-  [Activity(Label = "MobileTsar", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+  [Activity(Label = "Tsar Mobile", Icon = "@drawable/TransTusks", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
   public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
   {
     protected override void OnCreate(Bundle bundle)
---
+++ MobileTsar/MobileTsar.Droid/Resources/values/SplashActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MobileTsar.Droid.Resources.values
+{
+    //[Activity(Label = "SplashActivity")]
+    [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            System.Threading.Thread.Sleep(2); // Simulate a long loading process on app      
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
