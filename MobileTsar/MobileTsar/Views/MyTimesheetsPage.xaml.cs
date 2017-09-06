265c6af130aef378376639664ed64d94e9125721
Devashen Govender 21327444 <govenderdevashen9@gmail.com>
2017-09-06 14:50:53 +0200

    Amend my timesheets

--- MobileTsar/MobileTsar/Views/MyTimesheetsPage.xaml.cs
+++ MobileTsar/MobileTsar/Views/MyTimesheetsPage.xaml.cs
@@ -16,20 +16,31 @@ namespace MobileTsar.Views
   public partial class MyTimesheetsPage : ContentPage
   {
     public ObservableCollection<Timesheet> Items { get; set; }
-
+    
     public MyTimesheetsPage()
     {
       InitializeComponent();
+      
       GetTimesheets();
       Items = new ObservableCollection<Timesheet>();
       BindingContext = this;
+      SignedSwitch.IsVisible = false;
+      FilterLabel.IsVisible = false;
+      LoadingActivityIndicator.IsVisible = true;
+      SignedSwitch.IsToggled = false;
+      FilterLabel.Text = $"Viewing unsigned timesheets";
+      LoadingActivityIndicator.IsRunning = true;
+     
     }
+
+
     public async void GetTimesheets()
     {
+      LoadingActivityIndicator.IsRunning = true;
+      LoadingActivityIndicator.IsVisible = true;
       var api = new ApiServices();
       var token = Settings.AccessToken;
-      var list = await api.GetTimesheetAsync(token);
-
+      var list = await api.GetTimesheetAsync(token);     
       var usernamelist2 = await api.GetUsernameAsync(token);
       var currentuser = usernamelist2.Email;
       var consultantlist = await api.GetConsultantAsync(token);
@@ -37,18 +48,35 @@ namespace MobileTsar.Views
       var username = consultantlist.Where(t => t.ConsultantUserName == currentuser).Select(t => t.ConsultantNum).FirstOrDefault();
       //ConsultantIdLabel.Text = username.ToString();
 
-      //var ret = list.Select(t => t.TimesheetId).ToList();     
-      foreach (var timesheet in list.ToList().Where(t=>t.SignOff==false && t.ConsultantNum==username))
+      //var ret = list.Select(t => t.TimesheetId).ToList(); 
+      var signfilter = SignedSwitch.IsToggled;
+      Items.Clear();
+      foreach (var timesheet in list.ToList().Where(t=>t.SignOff==signfilter && t.ConsultantNum==username))
        {
         Items.Add(timesheet);
       };
-
+      if (SignedSwitch.IsToggled == true)
+      {
+        var count = Items.Count;
+        FilterLabel.Text = $"Viewing {count} signed timesheets";
+      }
+      else
+      {
+        var count = Items.Count;
+        FilterLabel.Text = $"Viewing {count} unsigned timesheets";
+      }
+      LoadingActivityIndicator.IsRunning = false;
+      LoadingActivityIndicator.IsVisible = false;
+      SignedSwitch.IsVisible = true;
+      FilterLabel.IsVisible = true;
+    
     }
 
 
     async void Handle_ItemTapped(object sender, ItemTappedEventArgs itemTappedEventArgs)
     {
       var timesheet = itemTappedEventArgs.Item as Timesheet;
+      
       if (itemTappedEventArgs.Item == null)
         return;
 
@@ -56,6 +84,22 @@ namespace MobileTsar.Views
 
       //Deselect Item
       ((ListView)sender).SelectedItem = null;
+    }
+
+    private void SignedSwitch_OnToggled(object sender, ToggledEventArgs e)
+    {
+      
+      if (SignedSwitch.IsToggled==true)
+      {
+        var count = Items.Count;
+        FilterLabel.Text = $"Viewing {count} signed timesheets";
+      }
+      else
+      {
+        var count = Items.Count;
+        FilterLabel.Text = $"Viewing {count} unsigned timesheets";
+      }
+      GetTimesheets();
     }
   }
 }
