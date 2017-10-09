0849b76aaace5d723cfe21ad679686cf9df56079
Keeolin <keeo20.naicker@gmail.com>
2017-10-09 22:19:10 +0200

    export to pdf

--- TSAR/Controllers/ManageController.cs
+++ TSAR/Controllers/ManageController.cs
@@ -6,6 +6,12 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using iTextSharp.text.html.simpleparser;
+
 using TSAR.Models;
 
 namespace TSAR.Controllers
@@ -407,6 +413,23 @@ namespace TSAR.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public FileResult Export(string GridHtml)
+        {
+            using (MemoryStream stream = new System.IO.MemoryStream())
+            {
+                StringReader sr = new StringReader(GridHtml);
+                Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 100f, 0f);
+                PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+                pdfDoc.Open();
+                XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+                pdfDoc.Close();
+                return File(stream.ToArray(), "application/pdf", "Report.pdf");
+            }
+        }
+
 
     }
 }
